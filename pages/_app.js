b1fd40f167d016fcd61d49a93afa5f745bd8f19d
philramirez <pramirez@tollbrothers.com>
2022-12-27 16:09:48 -0500

    fix: library menu for the home page

--- pages/_app.js
+++ pages/_app.js
@@ -13,8 +13,8 @@ export default function App ({ Component, pageProps, router }) {
 	const currentShowcase = showcase.map(item => item.split('.js')[0])
 	const currentComponentIndex = currentShowcase.findIndex((value) => value.toLowerCase() === component)
 	const currentComponent = currentShowcase[currentComponentIndex]
-	const [isLibraryVisible, setIsLibraryVisible] = useState(true)
 	const breadcrumbs = slug?.filter(slug => slug !== '') || []
+	const [isLibraryVisible, setIsLibraryVisible] = useState(breadcrumbs.length > 0)
 	return (
 		<Layout currentShowcase={currentShowcase} currentComponentIndex={currentComponentIndex} breadcrumbs={breadcrumbs}
 						isLibraryVisible={isLibraryVisible} setIsLibraryVisible={setIsLibraryVisible}>
