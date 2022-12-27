cb3ac9612b2eea7e2396ff4040c515378122283a
philramirez <pramirez@tollbrothers.com>
2022-12-27 16:02:39 -0500

    fix: library menu is visible by default

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ export default function App ({ Component, pageProps, router }) {
 	const currentShowcase = showcase.map(item => item.split('.js')[0])
 	const currentComponentIndex = currentShowcase.findIndex((value) => value.toLowerCase() === component)
 	const currentComponent = currentShowcase[currentComponentIndex]
-	const [isLibraryVisible, setIsLibraryVisible] = useState(false)
+	const [isLibraryVisible, setIsLibraryVisible] = useState(true)
 	const breadcrumbs = slug?.filter(slug => slug !== '') || []
 	return (
 		<Layout currentShowcase={currentShowcase} currentComponentIndex={currentComponentIndex} breadcrumbs={breadcrumbs}
