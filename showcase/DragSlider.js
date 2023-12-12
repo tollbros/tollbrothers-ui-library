50b836cf04749af6ead25765c06f17a1c68eb92e
Charles Niedringhaus <cniedringhaus@tollbrothersinc.com>
2023-12-12 16:09:23 -0500

    fix: slider fixes

--- showcase/DragSlider.js
+++ showcase/DragSlider.js
@@ -2,33 +2,37 @@ import { DragSlider } from '@tollbrothers/tollbrothers-ui'
 import { useState } from 'react'
 import styles from './dragSliderLayout.module.scss'
 
-export default function DragSliderTest () {
-
-
-    const [myNumber, setMyNumber] = useState(200000);
+export default function DragSliderTest() {
+    const [myNumber, setMyNumber] = useState(500000);
+    //const myNumber = 500000;
     const myMin = 10000;
     const myMax = 1000000;
+    //const step = 5000;
 
     return (
 
         <article>
 
             <aside className={styles.dragContainer}>
-                <h2>plain</h2>
-                <p>above</p>
+
                 <div>
-                <DragSlider
-                    step={5000}
-                    minValue={10000}
-                    maxValue={1000000}
-                    initalNum={500000}
-                />
+                <p>My Value: {myNumber}</p>
+                    <DragSlider
+                       step={1000}
+                       minValue={10000}
+                       maxValue={1000000}
+                       number={myNumber}
+                       setNumber={setMyNumber}
+                    />
+                    <div className={styles.minmax}>
+                    <span>min: {myMin}</span>
+                    <span>max: {myMax}</span>
                 </div>
-                <p>Below</p>
+                </div>
+
             </aside>
 
-
-            <aside className={`${styles.dragContainer} ${styles.wider}`}>
+            {/* <aside className={`${styles.dragContainer} ${styles.wider}`}>
                 <h2>with state</h2>
                 <div>
                 <p>My Value: {myNumber}</p>
@@ -36,7 +40,7 @@ export default function DragSliderTest () {
                     step={1000}
                     minValue={10000}
                     maxValue={1000000}
-                    number={myNumber}
+                    initalNum={myNumber}
                     setNumber={setMyNumber}
                 />
                 <div className={styles.minmax}>
@@ -44,11 +48,9 @@ export default function DragSliderTest () {
                     <span>max: {myMax}</span>
                 </div>
                 </div>
-            </aside>
+            </aside> */}
+
         </article>
-        
-
-
 
     )
 }
